484b21c02f2b1e212022530fa6a4cbc8d0c897bf
abhishekchauhan1503 <abhishekchauhan1503@gmail.com>
2013-03-21 13:57:35 -0700

    Update Database_Tables.sql

--- db/Database_Tables.sql
+++ db/Database_Tables.sql
@@ -1,9 +1,12 @@
+--Table to store different types of roles in the system (like user, admin etc)
 CREATE TABLE roles(
 role_id_pk INT,
 role_name VARCHAR(20),
 PRIMARY KEY(role_id_pk)
 );
 
+
+--Table to store user details
 CREATE TABLE users(
 user_id_pk VARCHAR(30),
 user_first_name VARCHAR(40),
@@ -14,6 +17,8 @@ FOREIGN KEY (user_role_fk) REFERENCES roles(role_id_pk),
 PRIMARY KEY(user_id_pk)
 );
 
+
+--Table to store team details
 CREATE TABLE teams(
 team_id_pk VARCHAR(20),
 team_name VARCHAR(30),
@@ -21,6 +26,10 @@ team_description VARCHAR(100),
 PRIMARY KEY(team_id_pk)
 );
 
+   ALTER TABLE `teams` CHANGE `team_id_pk` `team_id_pk` INT( 11 ) NOT NULL AUTO_INCREMENT 
+
+
+--Table to store the mapping of users to teams
 CREATE TABLE team_users(
 tu_user_id_pk_fk VARCHAR(30),
 tu_team_id_pk_fk VARCHAR(20),
@@ -29,6 +38,8 @@ FOREIGN KEY (tu_user_id_pk_fk) REFERENCES users(user_id_pk),
 FOREIGN KEY (tu_team_id_pk_fk) REFERENCES teams(team_id_pk)
 );
 
+
+--Table to store board details
 CREATE TABLE boards(
 board_id_pk VARCHAR(20),
 bt_id_fk VARCHAR(20),
@@ -38,6 +49,8 @@ PRIMARY KEY(board_id_pk),
 FOREIGN KEY(bt_id_fk) REFERENCES teams(team_id_pk)
 );
 
+
+--Table to store iteration details
 CREATE TABLE iterations(
 iteration_id_pk VARCHAR(30),
 iteration_start_date DATE,
@@ -49,12 +62,16 @@ PRIMARY KEY(iteration_id_pk),
 FOREIGN KEY(ib_id_fk) REFERENCES boards(board_id_pk)
 );
 
+
+--Table to store status type details
 CREATE TABLE statuses(
 status_id_pk VARCHAR(10),
 status_name VARCHAR(40),
 PRIMARY KEY(status_id_pk)
 );
 
+
+--Table to store task details
 CREATE TABLE tasks(
 task_id_pk VARCHAR(10),
 task_name VARCHAR(40),
@@ -71,12 +88,14 @@ FOREIGN KEY (ti_id_fk) REFERENCES iterations(iteration_id_pk),
 FOREIGN KEY (ts_id_fk) REFERENCES statuses(status_id_pk)
 );
 
+--Table to store details of which change types can be made on the table
 CREATE TABLE change_types(
 change_type_id_pk VARCHAR(5),
 change_type_name VARCHAR(30),
 PRIMARY KEY(change_type_id_pk)
 );
 
+--Table to store change logs
 CREATE TABLE task_action_log(
 tal_id_pk VARCHAR(5),
 tal_user_id_fk VARCHAR(30),
@@ -86,6 +105,7 @@ tal_task_id_fk VARCHAR(10),
 tal_old_attribute_value VARCHAR(200),
 tal_new_attribute_value VARCHAR(200),
 PRIMARY KEY(tal_id_pk),
+FOREIGN KEY tal_user_id_fk REFERENCES users(user_id_pk),
 FOREIGN KEY (tal_change_type_id_fk) REFERENCES change_types(change_type_id_pk),
 FOREIGN KEY (tal_task_id_fk) REFERENCES tasks(task_id_pk)
 );
