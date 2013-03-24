4bed66851d7d002d57e4a740555211fe5cc8a753
ramtinb <ramtinb@gmail.com>
2013-03-24 15:40:51 -0700

    xx

--- WebApp/board.php
+++ WebApp/board.php
@@ -81,6 +81,49 @@
 
 <script type="text/javascript">
 
+	<?php
+
+		$query = "SELECT task_id_pk, task_name, task_description, task_work_estimation, taks_responsible_person_fk FROM tasks";
+		$items = mysql_query($query);
+		$data = array();
+		while( $item = mysql_fetch_array($items) )
+		{
+			$data[] = $item;
+		}
+
+		$jsonData = json_encode($data);
+		echo "var boardData = {$jsonData}"
+
+	?>
+
+		var taskTemplate =  "<li class='portlet'> \
+			    					<div class='portlet-container'> \
+			 		   					<span class='ui-icon ui-icon-arrow-4-diag'></span> \
+			 							<input id='taskId' name='taskId' type='hidden' value='[taskIdVal]' > \
+					    				<input id='taskName' name='taskName' type='text' placeholder='Task Name' value='[taskNameVal]'> \
+	 									<input id='taskDesc' name='taskDesc' type='text' placeholder='Task Desc' value='[taskDesc]'> \
+	 									<input id='taskEstimate' name='taskEstimate' type='text' placeholder='Task Estimate (hour)' value='[taskEstimateVal]' > \
+	 									<input id='taskResponsibleName' name='taskResponsibleName' type='text' placeholder='Task Responsible' value='[taskResponsibleNameVal]' > \
+										<input id='taskResponsibleId' name='taskResponsibleId' type='hidden' value='[taskResponsibleIdVal]' > \
+	 	   							</div>  \
+	  						</li>";
+
+	function drawBoard()
+	{
+		for each (task in boardData)
+		{
+			//draw task;
+			var t = taskTemplate;
+			t = t.replace('[taskIdVal]',task['task_id_pk']);
+			t = t.replace('[taskNameVal]',task['task_name']);
+			t = t.replace('[taskDesc]',task['task_description']);
+			t = t.replace('[taskEstimateVal]',task['task_work_estimation']);
+			t = t.replace('[taskResponsibleIdVal]',task['taks_responsible_person_fk']);
+			
+			$('#toDo').append(t);			
+		}
+	}	
+						
 	$(function() {
 		
 		$('#addNewTask').button({icons:{primary:"ui-icon-document"}});
@@ -88,6 +131,24 @@
 		$('#saveBoard').button({icons:{primary:"ui-icon-disk"}});
 		$("#iterationSelector").combobox();	
 	
+			
+		//load board
+		drawBoard();
+			
+		//Save board		
+		$('#saveBoard').click(function() {			
+			$('#toDo li').each(function(){			
+				d =  {
+				  	"task_id_pk" : $(this).find('#taskId').val(),
+					"task_id_pk" : $(this).find('#taskId').val(),
+					"task_name"  : $(this).find('#taskName').val(),
+				    "task_description" :  $(this).find('#taskDesc').val(),
+					"task_work_estimation" : $(this).find('#taskEstimate').val(),
+					"taks_responsible_person_fk" : $(this).find('#taskResponsibleId').val()
+				 };				
+				$.post('saveBoard.php', d, function(data){ console.log(data)});			
+			});
+		});
 			 		
 		
 		$('.taskContainer').sortable({
@@ -99,29 +160,24 @@
 		// $( ".taskContainer" ).disableSelection();
 		
 
-		$('#saveBoard').click(function() {
-
-		});
+		
 		
 		$('#addNewTask').click(function(){
-
-			$('#toDo').append(taskTemplate);
 			
-		});
-		
-		var taskTemplate =  "<li class='portlet'>";							
-			taskTemplate +=    "<div class='portlet-container'>";
-			taskTemplate += 		   "<span class='ui-icon ui-icon-arrow-4-diag'></span>";
-			taskTemplate += 			"<input id='taskId' name='taskId' type='hidden' >";
-			taskTemplate += 			"<input id='taskName' name='taskName' type='text' placeholder='Task Name'>";
-			taskTemplate += 			"<input id='taskDesc' name='taskDesc' type='text' placeholder='Task Desc'>";
-			taskTemplate += 			"<input id='taskEstimate' name='taskEstimate' type='text' placeholder='Task Estimate (hour)' >";
-			taskTemplate += 			"<input id='taskResponsible' name='taskResponsible' type='text' placeholder='Task Responsible' >";		
-			taskTemplate += 	   "</div>"
-			taskTemplate +=  "</li>";												
-		
-	});
-	
+			var t = taskTemplate;
+			t = t.replace('[taskIdVal]', '-1');
+			t = t.replace('[taskNameVal]','');
+			t = t.replace('[taskDesc]','');
+			t = t.replace('[taskEstimateVal]','');
+			t = t.replace('[taskResponsibleNameVal]','');
+			t = t.replace('[taskResponsibleIdVal]','-1');
+
+			$('#toDo').append(t);
+			
+		});															
+			
+					
+	});					
 	 
 
 </script>
@@ -134,9 +190,14 @@
 
 						
 						<select id="iterationSelector" name="iterationID" style="display:none;">
-							<option value="1">Iteration : 1</option>
-							<option value="2">Iteration : 2</option>
-							<option value="3">Iteration : 3</option>												
+							<?php
+								$squery= "SELECT * FROM iterations order by iteration_start_date desc "; //$_SESSION['team_num']
+								$sitems = mysql_query($query) or dire (mysql_error());
+								while ( $sitem = mysql_fetch_array($sitems) )
+								{
+									echo "<option value='1'> {$sitem['iteration_number']} : {$sitem['iteration_start_date']} - {$sitem['iteration_end_date']}  </option>";
+								}
+							?>
 						</select>
 
 					<div style="float:right">
